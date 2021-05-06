435247411da8e51cdcd7b76e527756f7e6818bce
Sandro Scalco <sandro.scalco@liitu.ch>
2021-05-06 23:04:29 +0200

    feat tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit', // https://tailwindcss.com/docs/just-in-time-mode
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')
+,require('@tailwindcss/forms')
+,require('@tailwindcss/line-clamp')
+,require('@tailwindcss/typography')
+],
+};
