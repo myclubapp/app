c484bd83b35053b812277bff819c77187e1af1d8
Sandro Scalco <sandro.scalco@liitu.ch>
2021-05-13 22:43:15 +0200

    feat login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,10 +1,10 @@
 import {PushNotificationService} from './../../services/push-notification.service';
-import {MenuController} from '@ionic/angular';
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {UserCredential} from 'src/app/models/user';
 import {AuthService} from 'src/app/services/auth.service';
-import {AuthFormComponent} from 'src/app/components/auth-form/auth-form.component';
 import {Router} from '@angular/router';
+import {MenuController, LoadingController} from '@ionic/angular';
+import {FormGroup, Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,25 +12,60 @@ import {Router} from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  @ViewChild(AuthFormComponent) loginForm: AuthFormComponent;
+  public authForm: FormGroup;
   constructor(
     private authService: AuthService,
     private router: Router,
     public menuCtrl: MenuController,
+    private formBuilder: FormBuilder, private loadingCtrl: LoadingController,
     private pushNotificationService: PushNotificationService
   ) {
     this.menuCtrl.enable(false, 'menu');
-  }
+
+    this.authForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.minLength(6)],
+    });
+}
 
   ngOnInit() {
     this.menuCtrl.enable(false, 'menu');
   }
 
+
+  submitCredentials(authForm: FormGroup): void {
+    if (!authForm.valid) {
+      console.log('Form is not valid yet, current value:', authForm.value);
+    } else {
+      this.presentLoading();
+      const credentials: UserCredential = {
+        email: authForm.value.email,
+        password: authForm.value.password,
+      };
+    
+      this.loginUser(credentials);
+
+    }
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingCtrl.create({
+      cssClass: 'my-custom-class',
+      message: 'Please wait...',
+      duration: 2000
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+    console.log('Loading dismissed!');
+  }
+
+
   async loginUser(credentials: UserCredential): Promise<void> {
     try {
       const userCredential: firebase.default.auth.UserCredential = await this.authService.login(credentials.email, credentials.password);
       this.authService.userId = userCredential.user.uid;
-      await this.loginForm.hideLoading();
+//      await this.loginForm.hideLoading();
 
       this.router.navigateByUrl('/home').catch((error) => {
         console.log(error.message);
@@ -40,8 +75,8 @@ export class LoginPage implements OnInit {
       this.pushNotificationService.requestPermission();
     } catch (error) {
       console.log(error);
-      await this.loginForm.hideLoading();
-      this.loginForm.handleError(error);
+      //await this.loginForm.hideLoading();
+      //this.loginForm.handleError(error);
     }
   }
 }
