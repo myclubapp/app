a761e34b8dddd0d2f7f4d53f4ff50a9c1a5285e2
Sandro Scalco <sandro.scalco@liitu.ch>
2021-05-14 12:17:55 +0200

    feat deps

--- src/app/models/training.ts
+++ src/app/models/training.ts
@@ -1,13 +1,12 @@
-
 export interface Training {
-    title: string;
-    description:  string;
-    location:  string;
-    timeFrom:  string;
-    timeTo:  string;
-    weekday:  string;
-    startDate:  string;
-    repeat:  string;
-    clubId:  string;
-    teamId:  string;
-  };
+  title: string;
+  description: string;
+  location: string;
+  timeFrom: string;
+  timeTo: string;
+  weekday: string;
+  startDate: string;
+  repeat: string;
+  clubId: string;
+  teamId: string;
+}
