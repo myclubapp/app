afc5288dd812d76ef2c6ec6fe15c7cb19cf13e65
Sandro Scalco <sandro.scalco@liitu.ch>
2021-05-17 16:01:14 +0200

    feat update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,10 @@ import {Component} from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
-import {PushNotificationService} from './services/push-notification.service';
-
 import {AlertController} from '@ionic/angular';
 import {SwUpdate} from '@angular/service-worker';
-import {AuthService} from './services/auth.service';
+//import {AuthService} from './services/auth.service';
 
-import {Plugins, PushNotification, PushNotificationToken, PushNotificationActionPerformed} from '@capacitor/core';
-
-const {PushNotifications} = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -20,14 +15,14 @@ const {PushNotifications} = Plugins;
 })
 export class AppComponent {
   constructor(
-    private authService: AuthService,
+    //private authService: AuthService,
     private swUpdate: SwUpdate,
     private alertController: AlertController,
-    private pushNotificationService: PushNotificationService
+
   ) {
     this.initializeApp();
 
-    firebase.default.auth().onAuthStateChanged(function (user) {
+    firebase.default.auth().onAuthStateChanged((user)=> {
       if (user) {
         // User is signed in.
         console.log('User is signed in.');
@@ -48,12 +43,12 @@ export class AppComponent {
     firebase.default
       .firestore()
       .enablePersistence()
-      .catch(function (err) {
-        if (err.code == 'failed-precondition') {
+      .catch((err)=> {
+        if (err.code === 'failed-precondition') {
           // Multiple tabs open, persistence can only be enabled
           // in one tab at a a time.
           // ...
-        } else if (err.code == 'unimplemented') {
+        } else if (err.code === 'unimplemented') {
           // The current browser does not support all of the
           // features required to enable persistence
           // ...
@@ -62,51 +57,12 @@ export class AppComponent {
     // Subsequent queries will use persistence, if it was enabled successfully
   }
 
-  ngOnInit() {
-    PushNotifications.requestPermission().then((result) => {
-      if (result.granted) {
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Show some error
-      }
-    });
-
-    // On success, we should be able to receive notifications
-    PushNotifications.addListener('registration', (token: PushNotificationToken) => {
-      alert('Push registration success, token: ' + token.value);
-    });
-
-    // Some issue with our setup and push will not work
-    PushNotifications.addListener('registrationError', (error: any) => {
-      alert('Error on registration: ' + JSON.stringify(error));
-    });
-
-    // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener('pushNotificationReceived', (notification: PushNotification) => {
-      alert('Push received: ' + JSON.stringify(notification));
-    });
-
-    // Method called when tapping on a notification
-    PushNotifications.addListener('pushNotificationActionPerformed', (notification: PushNotificationActionPerformed) => {
-      alert('Push action performed: ' + JSON.stringify(notification));
-    });
-
-    /*this.profileService.getUserProfile().then(userProfileSnapshot => {
-      if (userProfileSnapshot && userProfileSnapshot.data()) {
-        this.displayName = userProfileSnapshot.data().firstName;
-      }
-    });*/
-  }
-
   initializeApp(): void {
     if (this.swUpdate.available) {
       this.swUpdate.available.subscribe(() => {
         this.presentAlert();
       });
     }
-    this.pushNotificationService.requestPermission();
-    this.pushNotificationService.receiveMessage();
   }
 
   async presentAlert() {
