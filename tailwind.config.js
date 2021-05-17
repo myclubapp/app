6f75f5bf24741e09fab8b004605ff9d9783a76a2
Sandro Scalco <sandro.scalco@liitu.ch>
2021-05-17 15:23:02 +0200

    feat lot of stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,8 @@ module.exports = {
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-
-    },
-    colors:{
+    extend: {},
+    colors: {
       primary: {
         default: 'var(--ion-color-primary)',
         shade: 'var(--ion-color-primary-shade)',
@@ -35,7 +33,7 @@ module.exports = {
         default: 'var(--ion-color-danger)',
         shade: 'var(--ion-color-danger-shade)',
         tint: 'var(--ion-color-danger-tint)',
-      } ,
+      },
       light: {
         default: 'var(--ion-color-light)',
         shade: 'var(--ion-color-light-shade)',
@@ -50,13 +48,11 @@ module.exports = {
         default: 'var(--ion-color-dark)',
         shade: 'var(--ion-color-dark-shade)',
         tint: 'var(--ion-color-dark-tint)',
-      }
-    }
+      },
+    },
   },
   variants: {
-    extend: {
-      
-    },
+    extend: {},
   },
   plugins: [require('@tailwindcss/forms')],
 };
