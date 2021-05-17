4d40e72f89018da1fc6034dda2cede95033dcfea
Sandro Scalco <sandro.scalco@liitu.ch>
2021-05-17 16:10:15 +0200

    feat prod

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
-import {environment} from './environments/environment';
+import {environment} from './environments/environment.prod';
 
 import {defineCustomElements} from '@ionic/pwa-elements/loader';
 import {defineCustomElements as defineCustomElementsGoogleMaps} from 'web-google-maps/dist/loader';
