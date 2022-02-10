6a4099b33c904248b1bdb9254a9fdb9b797a0744
Sandro Scalco <sandro.scalco@liitu.ch>
2022-02-10 17:40:56 +0100

    feat swupdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
-
+import {SwUpdate} from '@angular/service-worker';
+import { AlertController, ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private swUpdate: SwUpdate,
+    private alertController: AlertController,
+    public modalController: ModalController,
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp(): void {
+    if (this.swUpdate.available) {
+      this.swUpdate.available.subscribe(() => {
+        this.presentAlert();
+      });
+    }
+  }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      //cssClass: 'my-custom-class',
+      header: 'Neue Version',
+      message: 'Eine neue Version ist verfügbar. Neue Version laden?',
+      buttons: [
+        {
+          text: 'Abbrechen',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            //console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'Laden',
+          handler: () => {
+            window.location.reload();
+            //console.log('Confirm Okay');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
