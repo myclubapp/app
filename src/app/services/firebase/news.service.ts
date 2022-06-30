70416b173daf2591e14665068ded011fcb6b44f7
Sandro Scalco <sandro.scalco@liitu.ch>
2022-06-30 09:07:22 +0200

    feat order

--- src/app/services/firebase/news.service.ts
+++ src/app/services/firebase/news.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { limit, Timestamp } from "firebase/firestore";
 import {
   Firestore, addDoc, collection, collectionData,
-  doc, docData, deleteDoc, updateDoc, DocumentReference, setDoc, query, where
+  doc, docData, deleteDoc, updateDoc, DocumentReference, setDoc, query, where, orderBy
 } from '@angular/fire/firestore';
 import { Observable, Observer } from 'rxjs';
 
@@ -23,7 +23,7 @@ export class NewsService {
   getNewsRef(): Observable<News[]> {
     // console.log(`Read Team Events List Ref ${teamId}`)
     const newssRefList = collection(this.firestore, `news`);
-    const q = query(newssRefList, where("date", ">=", new Date(Date.now() - 1000 * 3600 * 24 * 20).toISOString())); // heute - 20 Tage
+    const q = query(newssRefList, orderBy("date", "desc"), where("date", ">=", new Date(Date.now() - 1000 * 3600 * 24 * 20).toISOString())); // heute - 20 Tage
     return collectionData(q,  { idField: 'id' }) as unknown as Observable<News[]>;
   }
 }
