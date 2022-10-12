89b6ce74baf9aae5405f6afff8fd57b3d23f31fb
Sandro <sandro.scalco@gmail.com>
2022-10-12 16:06:07 +0200

    fat

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.myclub.default',
-  appName: 'myclubdefault',
+  appName: 'myclub',
   webDir: 'www',
   bundledWebRuntime: false,
 };
