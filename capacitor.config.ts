a1c587cb4fef9e204f11a5dff12aa0663d4c5fc8
Sandro <sandro.scalco@gmail.com>
2022-10-12 19:17:14 +0200

    feat default remove

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.myclub.default',
+  appId: 'app.myclub',
   appName: 'myclub',
   webDir: 'www',
   bundledWebRuntime: false,
