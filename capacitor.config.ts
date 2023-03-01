8e23845f405ed7876be914d431496de4cc06c0fe
Sandro <sandro.scalco@gmail.com>
2023-03-01 22:32:31 +0100

    feat ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.myclub',
+  appId: 'app.myclub.default',
   appName: 'myclub',
   webDir: 'www',
   bundledWebRuntime: false,
