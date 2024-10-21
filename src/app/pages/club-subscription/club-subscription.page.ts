05aa5a39020676ace4f7a9a88fabef33eb4a272e
sansan88 <sandro.scalco@liitu.ch>
2024-10-21 16:27:05 +0200

    feat: free abo

--- src/app/pages/club-subscription/club-subscription.page.ts
+++ src/app/pages/club-subscription/club-subscription.page.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { AlertController, ModalController,LoadingController, NavParams, ToastController } from '@ionic/angular';
+import { AlertController, ModalController, LoadingController, NavParams, ToastController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { User } from 'firebase/auth';
 import { Observable, catchError, combineLatest, defaultIfEmpty, forkJoin, from, lastValueFrom, map, of, switchMap, take, tap } from 'rxjs';
@@ -17,9 +17,10 @@ export class ClubSubscriptionPage implements OnInit {
   @Input("clubId") clubId: any;
   club$: Observable<any>;
 
+  clubMemberCount$: Observable<number>;
 
   user$: Observable<User>;
-  modules$ : Observable<any>;
+  modules$: Observable<any>;
   products$: Observable<any>;
   user: User;
   subscriptionStatus = "active";
@@ -43,71 +44,78 @@ export class ClubSubscriptionPage implements OnInit {
     this.club$ = this.getClub(this.clubId);
     this.products$ = this.getProductsAndPrices();
     this.modules$ = this.getModules();
+
+    this.clubMemberCount$ = this.fbService.getClubMemberRefs(this.clubId).pipe(
+      take(1),
+      map((members => members.length)),
+      tap((members) => console.log(members))
+    );
   }
 
   getClub(clubId: string) {
     return this.fbService.getClubRef(clubId).pipe(
-        switchMap((club) => {
-            if (!club) return of(null);
-            return this.fbService.getClubSubscriptionList(clubId).pipe(
-                map(subscriptions => subscriptions.sort((a, b) => b.created - a.created)),
-                switchMap((subscriptions) => {
-                    if (subscriptions.length === 0) {
-                        return of({
-                            ...club,
-                            activeSubscriptions: [],
-                            inactiveSubscriptions: []
-                        });
-                    }
-                    const subscriptionsWithDetails$ = subscriptions.map(subscription =>
-                        combineLatest([
-                            of(subscription),
-                            this.fbService.getClubSubscriptionInvoiceList(clubId, subscription.id).pipe(
-                                map(invoices => invoices.sort((a, b) => new Date(a.created).getTime() - new Date(b.created).getTime())),
-                                catchError(() => of([])), // Return empty array on error
-                                defaultIfEmpty([]) // Ensure empty array if no invoices are found
-                            ),
-                            this.fbService.getProduct(subscription.product.path.split('/')[1]).pipe(
-                                take(1),
-                                catchError(() => of({ id: subscription.product, name: "Unknown Product" })) // Return a default product on error
-                            )
-                        ]).pipe(
-                            map(([subscription, invoices, product]) => ({
-                                ...subscription,
-                                invoices,
-                                product
-                            }))
-                        )
-                    );
-                    return combineLatest(subscriptionsWithDetails$).pipe(
-                        map(subscriptionsWithDetails => ({
-                            ...club,
-                            activeSubscriptions: subscriptionsWithDetails.filter(sub => sub.status=='active'),
-                            inactiveSubscriptions: subscriptionsWithDetails.filter(sub => sub.status!=='active')
-                        }))
-                    );
-                }),
-                catchError(err => {
-                    console.error("Error fetching subscriptions:", err);
-                    return of({
-                        ...club,
-                        activeSubscriptions: [],
-                        inactiveSubscriptions: []
-                    });
-                })
+      switchMap((club) => {
+        if (!club) return of(null);
+        return this.fbService.getClubSubscriptionList(clubId).pipe(
+          map(subscriptions => subscriptions.sort((a, b) => b.created - a.created)),
+          switchMap((subscriptions) => {
+            if (subscriptions.length === 0) {
+              return of({
+                ...club,
+                activeSubscriptions: [],
+                inactiveSubscriptions: []
+              });
+            }
+            const subscriptionsWithDetails$ = subscriptions.map(subscription =>
+              combineLatest([
+                of(subscription),
+                this.fbService.getClubSubscriptionInvoiceList(clubId, subscription.id).pipe(
+                  map(invoices => invoices.sort((a, b) => new Date(a.created).getTime() - new Date(b.created).getTime())),
+                  catchError(() => of([])), // Return empty array on error
+                  defaultIfEmpty([]) // Ensure empty array if no invoices are found
+                ),
+                this.fbService.getProduct(subscription.product.path.split('/')[1]).pipe(
+                  take(1),
+                  catchError(() => of({ id: subscription.product, name: "Unknown Product" })) // Return a default product on error
+                )
+              ]).pipe(
+                map(([subscription, invoices, product]) => ({
+                  ...subscription,
+                  invoices,
+                  product
+                }))
+              )
             );
-        }),
-        catchError(err => {
-            console.error("Error in getClubWithSubscriptions:", err);
-            return of(null);
-        })
+            return combineLatest(subscriptionsWithDetails$).pipe(
+              take(1),
+              map(subscriptionsWithDetails => ({
+                ...club,
+                activeSubscriptions: subscriptionsWithDetails.filter(sub => sub.status == 'active'),
+                inactiveSubscriptions: subscriptionsWithDetails.filter(sub => sub.status !== 'active')
+              }))
+            );
+          }),
+          catchError(err => {
+            console.error("Error fetching subscriptions:", err);
+            return of({
+              ...club,
+              activeSubscriptions: [],
+              inactiveSubscriptions: []
+            });
+          })
+        );
+      }),
+      catchError(err => {
+        console.error("Error in getClubWithSubscriptions:", err);
+        return of(null);
+      })
     );
-}
+  }
 
-changeSegment(event){
-  console.log(event)
-  this.subscriptionStatus = event.detail.value;
-}
+  changeSegment(event) {
+    console.log(event)
+    this.subscriptionStatus = event.detail.value;
+  }
 
   getProductsAndPrices() {
     return this.fbService.getProducts().pipe(
@@ -117,6 +125,8 @@ changeSegment(event){
         }
         const productsWithPrices$ = products.map(product =>
           this.fbService.getPrices(product.id).pipe(
+            take(1),
+
             map(prices => ({
               ...product,
               prices: prices.map(price => ({
@@ -125,7 +135,8 @@ changeSegment(event){
                 unit_amount: price.unit_amount ? (price.unit_amount / 100).toFixed(2) : "0.00" // Format to two decimal places
               })).sort((a, b) => a.unit_amount - b.unit_amount) // Sorting prices based on the adjusted amount
             })),
-            tap(prices=>console.log(prices)),
+            tap(prices => console.log(prices)),
+
             catchError(err => {
               console.error(`Error fetching prices for product ${product.id}:`, err);
               return of({ ...product, prices: [] }); // Return product with empty price list on error
@@ -134,6 +145,16 @@ changeSegment(event){
         );
         return combineLatest(productsWithPrices$); // Combine all products with their prices into a single array
       }),
+
+
+      // Sorting the products based on stripe_metadata_max_users
+      map(products =>
+        products.sort((a, b) =>
+          Number(a['stripe_metadata_max_users']) - Number(b['stripe_metadata_max_users'])
+        )
+      ),
+
+
       catchError(err => {
         console.error("Error fetching products and prices:", err);
         return of([]); // Return an empty array on error
@@ -149,6 +170,7 @@ changeSegment(event){
         }
         const productsWithPrices$ = products.map(product =>
           this.fbService.getPrices(product.id).pipe(
+            take(1),
             map(prices => ({
               ...product,
               prices: prices.map(price => ({
@@ -157,7 +179,7 @@ changeSegment(event){
                 unit_amount: price.unit_amount ? (price.unit_amount / 100).toFixed(2) : "0.00" // Format to two decimal places
               })).sort((a, b) => a.unit_amount - b.unit_amount) // Sorting prices based on the adjusted amount
             })),
-            tap(prices=>console.log(prices)),
+            tap(prices => console.log(prices)),
             catchError(err => {
               console.error(`Error fetching prices for product ${product.id}:`, err);
               return of({ ...product, prices: [] }); // Return product with empty price list on error
@@ -182,49 +204,49 @@ changeSegment(event){
       buttons: [
         {
           id: "cancel",
-          text:  await lastValueFrom(this.translate.get("common.cancel")),
+          text: await lastValueFrom(this.translate.get("common.cancel")),
           role: "cancel"
-        },{
-        id: "ok",
-        text: await lastValueFrom(this.translate.get("common.ok")),
-        handler: async ()=>{
-          const loading = await this.loadingCtrl.create({
-            showBackdrop: false,
-            message: "Bitte warten",
-          })
-          loading.present();
-      
-          from(this.fbService.checkoutSubscription(this.clubId, price, product)).pipe(
-            switchMap(checkout => {
-              console.log('Checkout session created with ID:', checkout.id);
-              return this.fbService.getCheckoutSession(this.clubId, checkout.id);
-            }),
-            catchError(error => {
-              console.error('Error during checkout process:', error);
-              return of(null); // Handle errors or provide a fallback value
+        }, {
+          id: "ok",
+          text: await lastValueFrom(this.translate.get("common.ok")),
+          handler: async () => {
+            const loading = await this.loadingCtrl.create({
+              showBackdrop: false,
+              message: "Bitte warten",
             })
-          ).subscribe(checkoutSession => {
-            if (checkoutSession) {
-              console.log('Received checkout session data:', checkoutSession);
-              if (checkoutSession && checkoutSession.url) {
-                loading.dismiss();
-                this.openUrl(checkoutSession.url);
-      
+            loading.present();
+
+            from(this.fbService.checkoutSubscription(this.clubId, price, product)).pipe(
+              switchMap(checkout => {
+                console.log('Checkout session created with ID:', checkout.id);
+                return this.fbService.getCheckoutSession(this.clubId, checkout.id);
+              }),
+              catchError(error => {
+                console.error('Error during checkout process:', error);
+                return of(null); // Handle errors or provide a fallback value
+              })
+            ).subscribe(checkoutSession => {
+              if (checkoutSession) {
+                console.log('Received checkout session data:', checkoutSession);
+                if (checkoutSession && checkoutSession.url) {
+                  loading.dismiss();
+                  this.openUrl(checkoutSession.url);
+
+                }
+              } else {
+                console.log('No checkout session data received.');
               }
-            } else {
-              console.log('No checkout session data received.');
-            }
-          });
-        },
-      }
-     ],
+            });
+          },
+        }
+      ],
       header: "Abo kaufen",
       message: "Möchtest du das Abo für " + price.currency_upper + " " + price.unit_amount + " kaufen?",
     })
     alert.present();
 
 
-    
+
   }
   async checkoutAddon(price, product) {
     console.log(price);
@@ -234,54 +256,54 @@ changeSegment(event){
       buttons: [
         {
           id: "cancel",
-          text:  await lastValueFrom(this.translate.get("common.cancel")),
+          text: await lastValueFrom(this.translate.get("common.cancel")),
           role: "cancel"
-        },{
-        id: "ok",
-        text: await lastValueFrom(this.translate.get("common.ok")),
-        handler: async ()=>{
-          const loading = await this.loadingCtrl.create({
-            showBackdrop: false,
-            message: "Bitte warten",
-          })
-          loading.present();
-      
-          from(this.fbService.checkoutAddon(this.clubId, price, product)).pipe(
-            switchMap(checkout => {
-              console.log('Checkout session created with ID:', checkout.id);
-              return this.fbService.getCheckoutSession(this.clubId, checkout.id);
-            }),
-            catchError(error => {
-              console.error('Error during checkout process:', error);
-              return of(null); // Handle errors or provide a fallback value
+        }, {
+          id: "ok",
+          text: await lastValueFrom(this.translate.get("common.ok")),
+          handler: async () => {
+            const loading = await this.loadingCtrl.create({
+              showBackdrop: false,
+              message: "Bitte warten",
             })
-          ).subscribe(checkoutSession => {
-            if (checkoutSession) {
-              console.log('Received checkout session data:', checkoutSession);
-              if (checkoutSession && checkoutSession.url) {
-                loading.dismiss();
-                this.openUrl(checkoutSession.url);
-      
+            loading.present();
+
+            from(this.fbService.checkoutAddon(this.clubId, price, product)).pipe(
+              switchMap(checkout => {
+                console.log('Checkout session created with ID:', checkout.id);
+                return this.fbService.getCheckoutSession(this.clubId, checkout.id);
+              }),
+              catchError(error => {
+                console.error('Error during checkout process:', error);
+                return of(null); // Handle errors or provide a fallback value
+              })
+            ).subscribe(checkoutSession => {
+              if (checkoutSession) {
+                console.log('Received checkout session data:', checkoutSession);
+                if (checkoutSession && checkoutSession.url) {
+                  loading.dismiss();
+                  this.openUrl(checkoutSession.url);
+
+                }
+              } else {
+                console.log('No checkout session data received.');
               }
-            } else {
-              console.log('No checkout session data received.');
-            }
-          });
-        },
-      }
-     ],
+            });
+          },
+        }
+      ],
       header: "Abo kaufen",
       message: "Möchtest du das Abo für " + price.currency_upper + " " + price.unit_amount + " kaufen?",
     })
     alert.present();
 
 
-    
+
   }
 
-  hasAcitveSubscription(subscriptionList){
+  hasAcitveSubscription(subscriptionList) {
     // console.log(subscriptionList)
-    return subscriptionList.filter(subcription=>subcription.status === "active");
+    return subscriptionList.filter(subcription => subcription.status === "active");
   }
 
   async openUrl(url: string) {
