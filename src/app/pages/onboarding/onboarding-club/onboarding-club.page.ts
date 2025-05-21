86876c905f9f4cdeebb08122b85b7983b9330002
sansan88 <sandro.scalco@liitu.ch>
2025-05-21 09:50:47 +0200

    feat update pricing

--- src/app/pages/onboarding/onboarding-club/onboarding-club.page.ts
+++ src/app/pages/onboarding/onboarding-club/onboarding-club.page.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Browser } from '@capacitor/browser';
-import { AlertController, MenuController, ToastController } from '@ionic/angular';
-import { TranslateService } from '@ngx-translate/core';
-import { User } from 'firebase/auth';
-import { Observable, Subscription, catchError, lastValueFrom, map, of, switchMap, take, tap } from 'rxjs';
-import { Club } from 'src/app/models/club';
-import { Profile } from 'src/app/models/user';
-import { AuthService } from 'src/app/services/auth.service';
-import { FirebaseService } from 'src/app/services/firebase.service';
-import { UserProfileService } from 'src/app/services/firebase/user-profile.service';
+import { Component, OnInit } from "@angular/core";
+import { Browser } from "@capacitor/browser";
+import {
+  AlertController,
+  MenuController,
+  ToastController,
+} from "@ionic/angular";
+import { TranslateService } from "@ngx-translate/core";
+import { User } from "firebase/auth";
+import {
+  Observable,
+  Subscription,
+  catchError,
+  lastValueFrom,
+  map,
+  of,
+  switchMap,
+  take,
+  tap,
+} from "rxjs";
+import { Club } from "src/app/models/club";
+import { Profile } from "src/app/models/user";
+import { AuthService } from "src/app/services/auth.service";
+import { FirebaseService } from "src/app/services/firebase.service";
+import { UserProfileService } from "src/app/services/firebase/user-profile.service";
 
 @Component({
-    selector: 'app-onboarding-club',
-    templateUrl: './onboarding-club.page.html',
-    styleUrls: ['./onboarding-club.page.scss'],
-    standalone: false
+  selector: "app-onboarding-club",
+  templateUrl: "./onboarding-club.page.html",
+  styleUrls: ["./onboarding-club.page.scss"],
+  standalone: false,
 })
 export class OnboardingClubPage implements OnInit {
   clubListSV: Club[];
@@ -36,7 +50,7 @@ export class OnboardingClubPage implements OnInit {
     private readonly alertController: AlertController,
     private readonly profileService: UserProfileService,
     public readonly menuCtrl: MenuController,
-  ) { 
+  ) {
     this.menuCtrl.enable(false, "menu");
   }
 
@@ -44,16 +58,20 @@ export class OnboardingClubPage implements OnInit {
     this.menuCtrl.enable(false, "menu");
     this.clubListSU = [];
     this.clubListSV = [];
-    this.clubListSH = []; 
+    this.clubListSH = [];
     this.clubListST = [];
-    this.subscription = this.authService.getUser$().pipe(
-      take(1),
-      tap(user => this.user = user),
-      switchMap(user => user ? this.profileService.getUserProfile(user) : of(null))
-    ).subscribe(profile => {
-      this.userProfile$ = of(profile);
-    })
-
+    this.subscription = this.authService
+      .getUser$()
+      .pipe(
+        take(1),
+        tap((user) => (this.user = user)),
+        switchMap((user) =>
+          user ? this.profileService.getUserProfile(user) : of(null),
+        ),
+      )
+      .subscribe((profile) => {
+        this.userProfile$ = of(profile);
+      });
   }
 
   ngOnDestroy() {
@@ -61,7 +79,6 @@ export class OnboardingClubPage implements OnInit {
       this.subscription.unsubscribe();
     }
   }
-
 
   getUserProfile(): Observable<any> {
     // Replace 'any' with the actual type of the user profile
@@ -76,7 +93,7 @@ export class OnboardingClubPage implements OnInit {
       catchError((err) => {
         console.error("Error fetching user profile", err);
         return of(null); // Handle the error and return a default value
-      })
+      }),
     );
   }
 
@@ -84,24 +101,22 @@ export class OnboardingClubPage implements OnInit {
     console.log(club);
 
     if (club.active) {
-    
       const alert = await this.alertController.create({
         inputs: [
-
           {
-            name: 'parent',
-            type: 'checkbox',
+            name: "parent",
+            type: "checkbox",
             label: "Ich möchte als Elternteil eingetragen werden",
-            value: 'parent',
+            value: "parent",
             checked: false,
           },
         ],
         message:
           (await lastValueFrom(
-            this.translate.get("onboarding.do_you_want_to_join__club")
+            this.translate.get("onboarding.do_you_want_to_join__club"),
           )) + ` ${club.name}`,
         header: await lastValueFrom(
-          this.translate.get("onboarding.join__club")
+          this.translate.get("onboarding.join__club"),
         ),
         buttons: [
           {
@@ -117,19 +132,21 @@ export class OnboardingClubPage implements OnInit {
             handler: async (data: any) => {
               console.log(data);
               try {
-                await this.fbService.setClubRequest(club.id, this.user.uid, data.key === "parent")
+                await this.fbService.setClubRequest(
+                  club.id,
+                  this.user.uid,
+                  data.key === "parent",
+                );
                 await this.presentRequestToast(); // Anfrage gesendet
-                await this.presentRequestSentAlert(club.name);// Inform Admin and Logout alert
+                await this.presentRequestSentAlert(club.name); // Inform Admin and Logout alert
               } catch (err) {
                 console.log(err.message);
                 if (err.message === "Missing or insufficient permissions.") {
                   this.presentErrorAlert();
                 }
               }
-
             },
           },
-         
         ],
       });
       await alert.present();
@@ -137,27 +154,33 @@ export class OnboardingClubPage implements OnInit {
       console.log("dieser club existiert noch nicht");
       const alert = await this.alertController.create({
         header: "Club aktivieren",
-        message: "Dieser Club wurde noch nicht aktiviert. Möchtest du den Club aktivieren? Falls deine E-Mail Adresse bereits hinterlegt ist, werden wir deinen Club sofort aktivieren.",
+        message:
+          "Dieser Club wurde noch nicht aktiviert. Möchtest du den Club aktivieren? Falls deine E-Mail Adresse bereits hinterlegt ist, werden wir deinen Club sofort aktivieren.",
         // subHeader: "Fülle dazu ein Antragsformular aus",
-        buttons: [{
-          text: await lastValueFrom(this.translate.get("common.cancel")),
-          role: "cancel",
-          handler: () => {
-            console.log("abbrechen");
-            this.presentCancelToast();
-            
+        buttons: [
+          {
+            text: await lastValueFrom(this.translate.get("common.cancel")),
+            role: "cancel",
+            handler: () => {
+              console.log("abbrechen");
+              this.presentCancelToast();
+            },
           },
-        },
-        {
-          text: await lastValueFrom(this.translate.get("common.ok")),
-        
-          handler: async () => {
-            console.log("OK");
-            await this.fbService.setClubRequest(club.id, this.user.uid, false)
-            await this.presentRequestToast(); // Anfrage gesendet
-            await this.presentActivatetSentAlert(club.name); 
+          {
+            text: await lastValueFrom(this.translate.get("common.ok")),
+
+            handler: async () => {
+              console.log("OK");
+              await this.fbService.setClubRequest(
+                club.id,
+                this.user.uid,
+                false,
+              );
+              await this.presentRequestToast(); // Anfrage gesendet
+              await this.presentActivatetSentAlert(club.name);
+            },
           },
-        }]
+        ],
       });
       alert.present();
     }
@@ -165,7 +188,7 @@ export class OnboardingClubPage implements OnInit {
   async presentRequestToast() {
     const toast = await this.toastController.create({
       message: await lastValueFrom(
-        this.translate.get("onboarding.success__request_sent")
+        this.translate.get("onboarding.success__request_sent"),
       ),
       duration: 1500,
       position: "top",
@@ -177,7 +200,7 @@ export class OnboardingClubPage implements OnInit {
   async presentCancelToast() {
     const toast = await this.toastController.create({
       message: await lastValueFrom(
-        this.translate.get("onboarding.warning__action_canceled")
+        this.translate.get("onboarding.warning__action_canceled"),
       ),
       duration: 1500,
       position: "top",
@@ -191,11 +214,11 @@ export class OnboardingClubPage implements OnInit {
     const alert = await this.alertController.create({
       cssClass: "my-custom-class",
       header: await lastValueFrom(
-        this.translate.get("onboarding.success__application_sent")
+        this.translate.get("onboarding.success__application_sent"),
       ),
       // subHeader: "",
       message: await lastValueFrom(
-        this.translate.get("onboarding.success__application_sent_desc")
+        this.translate.get("onboarding.success__application_sent_desc"),
       ),
       buttons: [
         {
@@ -212,18 +235,18 @@ export class OnboardingClubPage implements OnInit {
     const { role } = await alert.onDidDismiss();
     console.log("onDidDismiss resolved with role", role);
   }
-
-
 
   async presentActivatetSentAlert(clubName: string) {
     const alert = await this.alertController.create({
       cssClass: "my-custom-class",
       header: await lastValueFrom(
-        this.translate.get("onboarding.success__activate_pplication_sent")
+        this.translate.get("onboarding.success__activate_pplication_sent"),
       ),
       // subHeader: "",
       message: await lastValueFrom(
-        this.translate.get("onboarding.success__activate_application_sent_desc")
+        this.translate.get(
+          "onboarding.success__activate_application_sent_desc",
+        ),
       ),
       buttons: [
         {
@@ -241,22 +264,20 @@ export class OnboardingClubPage implements OnInit {
     console.log("onDidDismiss resolved with role", role);
   }
 
-
   async presentErrorAlert() {
     const alert = await this.alertController.create({
       cssClass: "my-custom-class",
       header: await lastValueFrom(
-        this.translate.get("onboarding.error__clubRequest")
+        this.translate.get("onboarding.error__clubRequest"),
       ),
       subHeader: "",
       message: await lastValueFrom(
-        this.translate.get("onboarding.error__clubRequest_desc")
+        this.translate.get("onboarding.error__clubRequest_desc"),
       ),
       buttons: [
         {
           text: await lastValueFrom(this.translate.get("common.ok")),
-          handler: async () => {
-          },
+          handler: async () => {},
         },
       ],
     });
@@ -278,13 +299,14 @@ export class OnboardingClubPage implements OnInit {
         .searchClubListRef(event.detail.value)
         .pipe(
           take(1),
+          // tap(clubs => console.log(clubs.length)),
           map((clubs: Club[]) =>
             clubs.filter((searchClub) =>
               searchClub.name
                 .toLowerCase()
-                .includes(event.detail.value.toLowerCase())
-            )
-          )
+                .includes(event.detail.value.toLowerCase()),
+            ),
+          ),
           // return club.name.search(searchValue)
         )
         .subscribe((data: any) => {
@@ -306,5 +328,4 @@ export class OnboardingClubPage implements OnInit {
   async logout() {
     return this.authService.logout();
   }
-
 }
